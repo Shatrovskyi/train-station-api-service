773c8fed40d256bc06e0e21d2986b5e1a2c9c517
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-12-11 13:46:44 +0200

    Implemented basic admin.py for train_station

--- train_station/admin.py
+++ train_station/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from train_station.models import (
+    Train,
+    Journey,
+    Station,
+    Route,
+    TrainType,
+    Crew,
+    Order,
+    Ticket,
+)
+
+
+admin.site.register(Train)
+admin.site.register(Journey)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(TrainType)
+admin.site.register(Crew)
+admin.site.register(Order)
+admin.site.register(Ticket)
