ae99fd6aa9419797784ade7c051446976ba8a936
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-12-12 11:22:41 +0200

    Fixed formating with black

--- train_station_api_service/urls.py
+++ train_station_api_service/urls.py
@@ -24,14 +24,16 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/train_station/", include("train_station.urls", namespace="train_station")),
+    path(
+        "api/train_station/", include("train_station.urls", namespace="train_station")
+    ),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
-        ),
+    ),
     path(
         "api/doc/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
